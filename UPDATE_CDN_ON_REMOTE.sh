70b31900462cf8ed19f3c3fc1ef5444fb6729a3a
GUFU <112747189+goophoo@users.noreply.github.com>
2022-09-03 17:47:49 +0800

    Update UPDATE_CDN_ON_REMOTE.sh

--- UPDATE_CDN_ON_REMOTE.sh
+++ UPDATE_CDN_ON_REMOTE.sh
@@ -11,8 +11,8 @@ uci set openclash.@servers[1].server=$a && \
 uci set openclash.@servers[2].server=$b && \
 uci set openclash.@servers[3].server=$b && \
 uci commit && \
-sed -i "w s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$c|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
-sed -i "x s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$c|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
-sed -i "y s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$d|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
-sed -i "z s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$d|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
+#sed -i "w s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$c|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
+#sed -i "x s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$c|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
+#sed -i "y s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$d|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
+#sed -i "z s|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}|$d|" /etc/openclash/proxy_provider/PROXY_PROVIDER.yaml && \
 /etc/init.d/openclash restart
